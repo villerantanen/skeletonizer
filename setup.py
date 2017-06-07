f05f7e10b32f4ed8829580202c0b951914f13b25
Ville Rantanen <ville.rantanen@reaktor.com>
2017-06-07 12:04:40 +0300

    setup tools

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+  name = 'skeletonizer',
+  packages = ['skeletonizer'], 
+  scripts = [],
+  version = '0.1',
+  description = 'Skeletonizer test',
+  author = 'Ville Rantanen',
+  author_email = 'ville.rantanen@reaktor.com',
+  url = 'https://github.com/villerantanen/skeletonizer/',
+  download_url = 'https://github.com/villerantanen/skeletonizer/archive/master.zip', 
+  keywords = ['opencv'], 
+  classifiers = [],
+  license = 'GPL',
+)
