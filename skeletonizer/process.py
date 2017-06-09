0b298834c127c89d15bef127b92d7b4e31abe8fb
Ville Rantanen <ville.rantanen@reaktor.com>
2017-06-09 12:58:23 +0300

    switch to skimage

--- skeletonizer/process.py
+++ skeletonizer/process.py
@@ -4,79 +4,53 @@ from __future__ import print_function
 from __future__ import division
 # import matplotlib.pyplot as plt
 import numpy as np
-import cv2
 import sys
+import skimage.io, skimage.filters, skimage.feature, skimage.morphology
+
+
 if sys.version_info[0] == 2:
     from urllib import urlopen
 else:
     from urllib.request import urlopen
 
-def url_to_image(url, readFlag=cv2.IMREAD_COLOR):
+def url_to_image(url):
     # download the image, convert it to a NumPy array, and then read
-    # it into OpenCV format
-    # Function is from opencvutils, copied because importing the lib requires Camera
-    resp = urlopen(url)
-    image = np.asarray(bytearray(resp.read()), dtype="uint8")
-    image = cv2.imdecode(image, readFlag)
+    image=skimage.io.imread(url, as_grey=True)
 
     # return the image
     return image
 
 def auto_canny(image, sigma = 0.35):
-    # compute the mediam of the single channel pixel intensities
-    v = np.median(image)
-
-    # apply automatic Canny edge detection using the computed median
-    lower = int(max(0, (1.0 - sigma) * v))
-    upper = int(min(255, (1.0 + sigma) *v))
-    edged = cv2.Canny(image, lower, upper)
+    # compute the median of the single channel pixel intensities
+    edged = skimage.feature.canny(image,sigma=sigma)
 
     # return edged image
     return edged
 
-def skeletonize(image,size=(3,3)):
+def skeletonize(image):
     # return a thin skeleton of thresholded shape
-    image_size = np.size(image)
-    skel = np.zeros(image.shape,np.uint8)
-
     if (image==0).all() or (image==255).all():
         # Image is empty, skeletonization impossible
-        return skel
+        return np.zeros(image.shape,np.uint8)
 
-    element = cv2.getStructuringElement(cv2.MORPH_CROSS,size)
-    
-    while(True):
-        eroded = cv2.erode(image,element)
-        skel = cv2.bitwise_or(skel,
-                    cv2.subtract(image,
-                        cv2.dilate(eroded,element)
-                        )
-                    )
-        image = eroded.copy()
-        
-        zeros = image_size - cv2.countNonZero(image)
-        if zeros==image_size:
-            return skel
+    return skimage.morphology.skeletonize(image)
+
     
 def load_image(url):
     """ reads url and returns cv2 image """
     return url_to_image(url)
 
-def convert_image(image,invert=False,nhood=11):
-    """ convert BGR image to Binary 
+def convert_image(image,invert=False,adjust=1.0):
+    """ convert Gray to mask
         invert:  Dark pixels are object pixels
-        nhood:  Size of thresholding neighborhood
+        adjust:  multily result of Otsu
     """
-    gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-    threshold_method=cv2.THRESH_BINARY
+    cutoff=skimage.filters.threshold_otsu(image)
+    cutoff=int(adjust*cutoff)
     if invert:
-        threshold_method=cv2.THRESH_BINARY_INV
-    mask = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                    threshold_method,int(nhood),2)
-    
-    #cutoff, mask=cv2.threshold(gray, 0,255, threshold_method+cv2.THRESH_OTSU)
-    #cutoff=int(adjust*cutoff)
-    #cutoff, mask=cv2.threshold(gray, cutoff, 255, threshold_method)
+        mask=image<cutoff
+    else:
+        mask=image>cutoff
     return mask
 
 # plt.subplot(1,3,1)
